64f5afff924e8b8ae1eadc261bf49e8fcfffc5a7
Smartboy320 <mcpeboy320@gmail.com>
2016-02-02 15:51:25 -0500

    Create GameWaitingTask.php

---
+++ src/Tasks/GameWaitingTask.php
@@ -0,0 +1,33 @@
+ <?php 
+ namespace src\Tasks; 
+  
+  
+ use pocketmine\scheduler\PluginTask; 
+ use pocketmine\level\Level; 
+ use pocketmine\Player; 
+ use pocketmine\Server; 
+ use pocketmine\tile\Sign; 
+ use pocketmine\scheduler\Task; 
+ use pocketmine\scheduler\ServerScheduler; 
+ use pocketmine\level\Position; 
+ use pocketmine\math\Vector3; 
+  
+ class GameWaitingTask extends PluginTask 
+ { 
+  
+     public function __construct(\MCrafters\TeamPvP\GameManager $plugin) 
+     { 
+         parent::__construct($plugin); 
+         $this->plugin = $plugin; 
+     } 
+  
+     public function onRun($tick) 
+     { 
+         foreach ($this->plugin->reds as $r) { 
+             foreach ($this->plugin->blues as $b) { 
+                 Server::getInstance()->getPlayer($r)->sendPopup("§eWaiting for players.."); 
+                 Server::getInstance()->getPlayer($b)->sendPopup("§eWaiting for players.."); 
+             } 
+         } 
+     } 
+ } 
